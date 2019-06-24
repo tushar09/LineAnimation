d6749b34e42179003f0b97a5b242f5dbd57156dd
Tushar <wtushar09@gmail.com>
2019-06-24 22:20:40 +0600

    some variables added

--- lineanimation/src/main/java/com/captaindroid/lineanimation/LineAnimation.java
+++ lineanimation/src/main/java/com/captaindroid/lineanimation/LineAnimation.java
@@ -1,11 +1,40 @@
 package com.captaindroid.lineanimation;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.captaindroid.lineanimation.utils.Coordinates;
+
+import java.util.ArrayList;
+
 public class LineAnimation extends View{
+
+    private Matrix matrix;
+    private Bitmap arrow;
+    private Path line;
+    private Paint paint;
+
+    ArrayList<Coordinates> coordinates;
+    ArrayList<Coordinates> coordinatesTan;
+    private int arrowX;
+    private int arrowY;
+    private int traveler = 0;
+
+    public boolean animateArrow = false;
+
+    private Context context;
+
+    private float arrowXTan;
+    private float arrowYTan;
+
+    private int speed = 9;
+
     public LineAnimation(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
---
+++ lineanimation/src/main/java/com/captaindroid/lineanimation/utils/Coordinates.java
@@ -0,0 +1,38 @@
+package com.captaindroid.lineanimation.utils;
+
+public class Coordinates {
+    int x, y;
+    float xf, yf;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public float getXf(){
+        return xf;
+    }
+
+    public void setXf(float xf){
+        this.xf = xf;
+    }
+
+    public float getYf(){
+        return yf;
+    }
+
+    public void setYf(float yf){
+        this.yf = yf;
+    }
+}
