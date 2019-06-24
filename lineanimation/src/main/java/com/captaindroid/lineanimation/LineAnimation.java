d89fcc3383bf1f8605ba149077d867f3c1165bf3
Tushar <wtushar09@gmail.com>
2019-06-24 22:17:09 +0600

    constructor added

--- lineanimation/src/main/java/com/captaindroid/lineanimation/LineAnimation.java
+++ lineanimation/src/main/java/com/captaindroid/lineanimation/LineAnimation.java
@@ -1,4 +1,12 @@
 package com.captaindroid.lineanimation;
 
-public class LineAnimation {
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class LineAnimation extends View{
+    public LineAnimation(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
 }
