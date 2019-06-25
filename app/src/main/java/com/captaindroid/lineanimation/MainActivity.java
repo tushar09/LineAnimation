95d10200f53fcfaf236dacebb9770018486e09c3
Tushar <wtushar09@gmail.com>
2019-06-25 18:48:51 +0600

    rocket drawable added and getter setter added

--- app/src/main/java/com/captaindroid/lineanimation/MainActivity.java
+++ app/src/main/java/com/captaindroid/lineanimation/MainActivity.java
@@ -1,14 +1,22 @@
 package com.captaindroid.lineanimation;
 
+import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    LineAnimation lineAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((LineAnimation)findViewById(R.id.la_view)).startAnimateArrow();
+        lineAnimation = findViewById(R.id.la_view);
+        Path p = new Path();
+        p.moveTo(lineAnimation.getWidth() / 2, 0);
+        p.cubicTo(0, lineAnimation.getHeight() / 2, lineAnimation.getWidth(), lineAnimation.getHeight() / 2, lineAnimation.getWidth() / 2, lineAnimation.getHeight());
+        lineAnimation.setPath(p);
+        lineAnimation.startAnimateArrow();
     }
 }
